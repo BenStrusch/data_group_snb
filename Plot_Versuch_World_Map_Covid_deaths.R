1ccac541aff6834b4dbd5189aab316e53630838a
BenStrusch <125275371+BenStrusch@users.noreply.github.com>
2023-03-23 10:57:54 +0100

    Aktualisierung

--- Plot_Versuch_World_Map_Covid_deaths.R
+++ Plot_Versuch_World_Map_Covid_deaths.R
@@ -29,7 +29,7 @@ deaths <- deathsraw %>% gather(key="date", value="deaths", -c(Country.Region, Pr
 recovered <- recoveredraw %>% gather(key="date", value="recovered", -c(Country.Region, Province.State, Lat, Long)) %>% group_by(Country.Region, date) %>% summarize(recovered=sum(recovered))
 
 
-country <-country[!(country$country=="Winter Olympics 2022" | country$country=="Summer Olympics 2020"),]
+country <- country[!(country$country=="Winter Olympics 2022" | country$country=="Summer Olympics 2020"),]
 country <- full_join(confirmed, deaths) %>% full_join(recovered)
 
 View(country)
@@ -80,3 +80,49 @@ world %>% select(-cumconfirmed) %>% gather("Type", "Cases", -c(date, days)) %>%
   theme_classic() +
   labs(title = "Covid-19 Global Cases", x= "Date", y= "Daily cases") +
   theme(plot.title = element_text(hjust = 0.5))
+
+
+
+p_cont <- ggplot(data = mpg) + 
+  geom_jitter(mapping = aes(x = displ, y = hwy, color = hwy)) +
+  theme_bw() + scale_color_distiller(palette = "Spectral") + labs(x = "Stringency", y = "Number of deaths", title="Covid Deaths vs  stringency measures", subtitle = "2021") + theme(legend.title = element_blank(), legend.position = "bottom")
+p_cont
+
+p_cont_2 <- ggplot(data = mpg) + 
+  geom_jitter(mapping = aes(x = displ, y = hwy, size = hwy, color=hwy)) +
+  theme_bw() + labs(x = "Stringency", y = "Number of deaths", title="Covid Deaths vs  stringency measures", subtitle = "2021") + theme(legend.title = element_blank(), legend.position = "bottom")
+p_cont_2
+
+stringency_x <- c(1,2,3,4,5,6,7,8,9,10)
+deaths_y <- c(10, 9 , 8, 7, 6, 5, 4, 3, 2, 1)
+happiness <- c(1,2,3,4,5,5,4,3,2,1)
+gdp_ex <- c(1,2,3,4,5,5,4,3,2,1)
+gdp_score <- gdp_ex * 10
+
+string_low <- runif(25, min=0, max = 25)
+deaths_new <- runif(50, min = 0, max = 10)
+happiness_new <- runif(50, min=0, max=5)
+gdp_sco <- runif(50,min=0, max=50)
+
+plot_example <- data.frame(stringency_x, deaths_y, happiness, gdp_score)
+plot_example
+
+plot_example_2 <- data.frame(string_x, deaths_new, happiness_new, gdp_sco)
+plot_example_2
+
+p_cont_2 <- ggplot(data = plot_example) + geom_jitter(mapping = aes(x = stringency_x, y = deaths_y, size = happiness, color=gdp_score)) +
+  theme_bw() + labs(x = "Stringency", y = "Number of deaths", title="Covid Deaths vs  stringency measures", subtitle = "2021") + theme(legend.position = "bottom")
+p_cont_2
+
+new_plot_2020 <- ggplot(data = plot_example_2) + geom_jitter(mapping = aes(x=string_x, y=deaths_new, size = happiness_new, color=gdp_sco)) +
+  theme_bw() + labs(x = "Stringency", y = "Number of deaths", title="Covid Deaths vs  stringency measures", subtitle = "2021") + theme(legend.position = "bottom")
+new_plot_2020
+
+library(readr)
+getwd()
+setwd("C:/Users/benja/OneDrive/code/data_group_snb")
+stringency_data <- read_lines("stringency_index_avg.txt", sep =";")
+View(stringency_data)
+
+stringency <- read_csv("OxCGRT_timeseries_all.csv")
+View(stringency)
